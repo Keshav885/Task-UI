d3367c748490f9c8b877a855b636b9fb20f7735d
Devops-anudeep <anudeep.belaganti@aurotechcorp.com>
2017-08-10 12:00:57 -0400

    Added modified screens and html

---
+++ sample_iod/js/actions.js
@@ -0,0 +1,30 @@
+< transformiix: result xmlns: transformiix = "http://www.mozilla.org/TransforMiix" >
+    var url = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js";
+$.getScript(url, function() {});
+
+var url2 = "https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.4/hammer.min.js";
+$.getScript(url2, function() {});
+
+$(window).load(function() {
+    // dynamic views logic
+
+    // add link events to widgets
+
+    // set src on masters
+
+    // set default styles on pages and masters
+
+    // setting style for  splash page
+    $("#irise715fa176575d457088cfe8db322d6467").find('*').addClass('NormalStyle');
+    // setting style for  Home
+    $("#irise24d2d0ace27b4dbdbf4c9f1149bc554c").find('*').addClass('NormalStyle');
+    // setting style for  Inner-1
+    $("#irise1224ee2fc81f4a24a9e155e8fea24b61").find('*').addClass('NormalStyle');
+    // set data from clipboard
+
+}); // end of page load logic 
+
+// create clipboard handlers
+
+// create form handlers
+< /transformiix:result>
---
+++ sample_iod/js/datasheet.js
@@ -0,0 +1,3 @@
+< transformiix: result xmlns: transformiix = "http://www.mozilla.org/TransforMiix" >
+    // datasheet JSON definitions
+    < /transformiix:result>
